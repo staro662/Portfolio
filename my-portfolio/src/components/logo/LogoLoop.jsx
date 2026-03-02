7f76efd1bce11c3bd18d83701e73b75adabab0b6
staro662 <mixiaofan992@gmail.com>
2026-03-02 19:41:26 +0800

    changed capitalization

--- my-portfolio/src/components/logo/LogoLoop.jsx
+++ my-portfolio/src/components/logo/LogoLoop.jsx
@@ -1,5 +1,5 @@
 import { useCallback, useEffect, useMemo, useRef, useState, memo } from 'react';
-import './LogoLoop.css';
+import './logoloop.css';
 
 const ANIMATION_CONFIG = {
     SMOOTH_TAU: 0.25,
